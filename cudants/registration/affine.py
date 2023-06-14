fixed gradient smoothing bug

cudants/registration/affine.py
@@ -18,13 +18,14 @@ class AffineRegistration(AbstractRegistration):
                 optimizer: str = 'SGD', optimizer_params: dict = {},
                 optimizer_lr: float = 0.1, optimizer_momentum: float = 0.0,
                 mi_kernel_type: str = 'b-spline', cc_kernel_type: str = 'rectangular',
+                cc_kernel_size: int = 3,
                 tolerance: float = 1e-6, max_tolerance_iters: int = 10, tolerance_mode: str = 'atol',
                 init_rigid: Optional[torch.Tensor] = None,
                 custom_loss: nn.Module = None,
                 blur: bool = True,
                 ) -> None:
 
-        super().__init__(scales, iterations, fixed_images, moving_images, loss_type, mi_kernel_type, cc_kernel_type, custom_loss,
+        super().__init__(scales, iterations, fixed_images, moving_images, loss_type, mi_kernel_type, cc_kernel_type, custom_loss, cc_kernel_size,
                          tolerance, max_tolerance_iters, tolerance_mode)
         device = self.device
         dims = self.dims

cudants/registration/logdemons.py
@@ -18,8 +18,7 @@ class LogDemonsRegistration(AbstractRegistration):
     '''
     This class implements multi-scale log-demons registration
 
-    Returns the warp field in pixel coordinates because optical flow works in pixel coordinates
-    TODO: Investigate if we can change this to physical coordinates 
+    Returns the warp field in pixel coordinates 
     '''    
     def __init__(self, scales: List[int], iterations: List[float], 
                 fixed_images: BatchedImages, moving_images: BatchedImages,
@@ -58,6 +57,7 @@ class LogDemonsRegistration(AbstractRegistration):
         self.eps_prime = eps_prime
         self.update_gaussian = gaussian_1d(torch.tensor(update_sigma, device=self.device), truncated=2, approx='sampled')
         self.velocity_fields = []
+        self.integrator_n = 10
 
     def _get_symmetric_velocity(self,
                               velocity_field, fixed_image_vgrid, fixed_image_affinecoords, 
@@ -66,8 +66,7 @@ class LogDemonsRegistration(AbstractRegistration):
         Get forward and backward velocities between the fixed and moving images, and average them out
         '''
         # forward velocity
-        displacement_field = scaling_and_squaring(velocity_field, fixed_image_vgrid, n=6)
-        # displacement_field = torch.einsum('nij,n...j->n...i', affine_map_inverse, displacement_field)
+        displacement_field = scaling_and_squaring(velocity_field, fixed_image_vgrid, n=self.integrator_n)
         total_displacement_coords = fixed_image_affinecoords + displacement_field
         moved_image = F.grid_sample(moving_image_down, total_displacement_coords, mode='bilinear', align_corners=True)  # [N, C, H, W, [D]]
         v1 = self.optical_flow(fixed_image_down, moved_image)  # [N, dims, H, W, D]
@@ -79,16 +78,16 @@ class LogDemonsRegistration(AbstractRegistration):
         # backward velocity (compute M(Ax + b), F(x - \phi(x)) and compute optical flow)
         moved_image_aff = F.grid_sample(moving_image_down, fixed_image_affinecoords, mode='bilinear', align_corners=True)
         # displacement_field_bwd = scaling_and_squaring(-torch.einsum('nij,n...j->n...i', affine_map_forward, velocity_field), fixed_image_vgrid, n=6)
-        displacement_field_bwd = scaling_and_squaring(-velocity_field, fixed_image_vgrid, n=6)
+        displacement_field_bwd = scaling_and_squaring(-velocity_field, fixed_image_vgrid, n=self.integrator_n)
         fixed_image_warp = F.grid_sample(fixed_image_down, fixed_image_vgrid + displacement_field_bwd, mode='bilinear', align_corners=True)
         v2 = self.optical_flow(moved_image_aff, fixed_image_warp)
-        # v2 = torch.einsum('nij,nj...->ni...', affine_map_inverse, v2)  # [N, dims, H, W, D]
         norm = v2.abs().flatten(1).max(1).values.reshape(-1, *([1]*(self.dims + 1))) + 1e-20
         v2 = v2 / norm * scale_factor
+        ## move this to moving image coordinates
+        v2 = F.grid_sample(v2, fixed_image_vgrid + displacement_field, mode='bilinear', align_corners=True)
         v2 = v2.permute(*permute_idx)
-
         # update velocity function
-        velocity_field = velocity_field + 0.5*(v1 + self.lie_bracket_fn(velocity_field, v1)) + 0.5*(-v2 + self.lie_bracket_fn(velocity_field, v2))
+        velocity_field = velocity_field + 0.5*(v1 + 0.5*self.lie_bracket_fn(velocity_field, v1)) + 0.5*(-v2 + 0.5*self.lie_bracket_fn(velocity_field, v2))
         cur_loss = F.mse_loss(moved_image, fixed_image_down)
         return velocity_field, cur_loss, moved_image
         
@@ -101,8 +100,7 @@ class LogDemonsRegistration(AbstractRegistration):
         velocity_field is integrated to get the displacement field, which is added to the affine map
         velocity is computed 
         '''
-        displacement_field = scaling_and_squaring(velocity_field, fixed_image_vgrid, n=6)
-        # displacement_field = torch.einsum('nij,n...j->n...i', affine_map_inverse, displacement_field)
+        displacement_field = scaling_and_squaring(velocity_field, fixed_image_vgrid, n=self.integrator_n)
         total_displacement_coords = fixed_image_affinecoords + displacement_field
         moved_image = F.grid_sample(moving_image_down, total_displacement_coords, mode='bilinear', align_corners=True)  # [N, C, H, W, [D]]
         v = self.optical_flow(fixed_image_down, moved_image)  # [N, dims, H, W, D]

cudants/registration/rigid.py
@@ -20,11 +20,12 @@ class RigidRegistration(AbstractRegistration):
                 optimizer_lr: float = 0.1, optimizer_momentum: float = 0.0,
                 mi_kernel_type: str = 'b-spline', cc_kernel_type: str = 'rectangular',
                 tolerance: float = 1e-6, max_tolerance_iters: int = 10, tolerance_mode: str = 'atol',
+                cc_kernel_size: int = 3,
                 init_translation: Optional[torch.Tensor] = None,
                 custom_loss: nn.Module = None, 
                 blur: bool = True,
                 ) -> None:
-        super().__init__(scales, iterations, fixed_images, moving_images, loss_type, mi_kernel_type, cc_kernel_type, custom_loss,
+        super().__init__(scales, iterations, fixed_images, moving_images, loss_type, mi_kernel_type, cc_kernel_type, custom_loss, cc_kernel_size,
                          tolerance, max_tolerance_iters, tolerance_mode)
         # initialize transform
         device = fixed_images.device

cudants/registration/syn.py
@@ -46,8 +46,6 @@ class SyNRegistration(AbstractRegistration):
         # get warp
         if optimizer == 'SGD':
             optimizer_params['momentum'] = optimizer_momentum
-        # else:
-            # del optimizer_params.get('momentum')
 
         if deformation_type == 'geodesic':
             fwd_warp = GeodesicShooting(fixed_images, moving_images, integrator_n=integrator_n, 
@@ -65,6 +63,7 @@ class SyNRegistration(AbstractRegistration):
         if init_affine is None:
             init_affine = torch.eye(self.dims+1, device=fixed_images.device).unsqueeze(0).repeat(fixed_images.size(), 1, 1)  # [N, D, D+1]
         self.affine = init_affine.detach()
+
 
     def optimize(self, save_transformed=False):
         ''' 
@@ -139,7 +138,7 @@ class SyNRegistration(AbstractRegistration):
                     fwd_warp_field = separable_filtering(fwd_warp_field.permute(*self.fwd_warp.permute_vtoimg), warp_gaussian).permute(*self.fwd_warp.permute_imgtov)
                     rev_inv_warp_field = separable_filtering(rev_inv_warp_field.permute(*self.rev_warp.permute_vtoimg), warp_gaussian).permute(*self.rev_warp.permute_imgtov)
                     # compose the two warp fields
-                    composed_warp = compose_warp(fwd_warp_field, rev_inv_warp_field, fixed_image_vgrid)  # compose two displacement fields with initial grid
+                    composed_warp = compose_warp(rev_inv_warp_field, fwd_warp_field, fixed_image_vgrid)  # compose two displacement fields with initial grid
                     moved_coords_final = fixed_image_affinecoords + composed_warp
                     moved_image = F.grid_sample(moving_image_blur, moved_coords_final, mode='bilinear', align_corners=True)
                     transformed_images.append(moved_image.detach().cpu())
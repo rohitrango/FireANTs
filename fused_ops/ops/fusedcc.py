20a733a2a689bdc3f1096989d2e1c669b1010da9
rohitrango <rohit.rango@gmail.com>
2025-03-17 11:15:03 -0400

    add ants gradient

--- fused_ops/ops/fusedcc.py
+++ fused_ops/ops/fusedcc.py
@@ -22,7 +22,7 @@ reduction_table = {
 
 class FusedNCC3d(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, input_img, target_img, kernel_size, nr, dr, reduction):
+    def forward(ctx, input_img, target_img, kernel_size, nr, dr, reduction, use_ants_gradient):
         reduction = reduction_table[reduction.lower()]
         B, C, H, W, D = input_img.shape
         interm = torch.zeros(B, 5 * C, H, W, D, device=input_img.device, dtype=input_img.dtype)
@@ -39,28 +39,45 @@ class FusedNCC3d(torch.autograd.Function):
         padding = (kernel_size - 1) // 2
         interm = F.conv3d(interm, avg_filt, padding=padding, stride=1, groups=interm.shape[1])
         out = ffo.cc3d_fwd_interm_v1(interm, int(kernel_vol), reduction, nr, dr)
-        ctx.save_for_backward(interm, input_img, target_img, out)
+        ctx.save_for_backward(interm, input_img, target_img, out, avg_filt * kernel_vol)
         ctx.kernel_size = kernel_size
         ctx.nr = nr
         ctx.dr = dr 
         ctx.reduction = reduction
+        ctx.use_ants_gradient = use_ants_gradient
         return out
     
     @staticmethod
     def backward(ctx, grad_output):
         # retrieve saved tensors
-        interm, input_img, target_img, out = ctx.saved_tensors
-        kernel_size, nr, dr, reduction = ctx.kernel_size, ctx.nr, ctx.dr, ctx.reduction
+        interm, input_img, target_img, out, avg_filt = ctx.saved_tensors
+        kernel_size, nr, dr, reduction, use_ants_gradient = ctx.kernel_size, ctx.nr, ctx.dr, ctx.reduction, ctx.use_ants_gradient
         B, C, H, W, D = input_img.shape
         # initialize gradients
         grad_input_img = None
         grad_target_img = None
+        # if backward is called, first tensor will always require grad
+        # second may or may not require grad
         if input_img.requires_grad:
             grad_input_img = torch.zeros(B, C, H, W, D, device=input_img.device, dtype=input_img.dtype)
         if target_img.requires_grad:
             grad_target_img = torch.zeros(B, C, H, W, D, device=input_img.device, dtype=input_img.dtype)
-        ffo.cc3d_bwd_interm_v1(interm, input_img, target_img, grad_output, grad_input_img, grad_target_img, kernel_size, nr, dr, reduction)
-        return grad_input_img, grad_target_img, None, None, None, None
+        # compute gradients
+        # compute correct gradients (maybe slightly slower)
+        ffo.cc3d_bwd_modify_interm_v1(interm, input_img, target_img, grad_output, grad_input_img, grad_target_img, kernel_size, nr, dr, reduction)
+        # convolve with average filter depending on whether grad_target_img is None
+        # if using ants_gradient, the convolution is skipped (ignore interactions from other neighboring pixels)
+        if not use_ants_gradient:
+            padding = (kernel_size - 1) // 2
+            if grad_target_img is not None:
+                interm = F.conv3d(interm, avg_filt, padding=padding, stride=1, groups=interm.shape[1])
+            else:
+                avg_filt = avg_filt[:3*C]
+                interm[:, :3*C, :, :, :] = F.conv3d(interm[:, :3*C, :, :, :], avg_filt, padding=padding, stride=1, groups=3*C)
+        # solve for grad_input_img and grad_target_img
+        ffo.cc3d_bwd_compute_grads(interm, input_img, target_img, grad_input_img, grad_target_img)
+
+        return grad_input_img, grad_target_img, None, None, None, None, None
 
 
 class FusedLocalNormalizedCrossCorrelationLoss(nn.Module):
@@ -79,6 +96,7 @@ class FusedLocalNormalizedCrossCorrelationLoss(nn.Module):
         reduction: str = "mean",
         smooth_nr: float = 0,
         smooth_dr: float = 1e-5,
+        use_ants_gradient: bool = False,
     ) -> None:
         """
         Args:
@@ -105,6 +123,7 @@ class FusedLocalNormalizedCrossCorrelationLoss(nn.Module):
         # _kernel = look_up_option(kernel_type, kernel_dict)
         self.smooth_nr = float(smooth_nr)
         self.smooth_dr = float(smooth_dr)
+        self.use_ants_gradient = use_ants_gradient
 
     def forward(self, pred: torch.Tensor, target: torch.Tensor, mask: Optional[torch.Tensor] = None) -> torch.Tensor:
         """
@@ -118,31 +137,26 @@ class FusedLocalNormalizedCrossCorrelationLoss(nn.Module):
             raise ValueError(f"expecting pred with {self.ndim} spatial dimensions, got pred of shape {pred.shape}")
         if target.shape != pred.shape:
             raise ValueError(f"ground truth has differing shape ({target.shape}) from pred ({pred.shape})")
-        return FusedNCC3d.apply(pred, target, self.kernel_size, self.smooth_nr, self.smooth_dr, self.reduction)
+        # check if both pred and target require grad
+        pgrad, tgrad = pred.requires_grad, target.requires_grad
+        # if both or neither require grad, dont shuffle the order 
+        if (pgrad and tgrad) or (not pgrad and not tgrad):
+            return FusedNCC3d.apply(pred, target, self.kernel_size, self.smooth_nr, self.smooth_dr, self.reduction, self.use_ants_gradient)
+        else:
+            # if only pred requires grad, swap pred and target
+            if pgrad:
+                return FusedNCC3d.apply(pred, target, self.kernel_size, self.smooth_nr, self.smooth_dr, self.reduction, self.use_ants_gradient)
+            else:
+                return FusedNCC3d.apply(target, pred, self.kernel_size, self.smooth_nr, self.smooth_dr, self.reduction, self.use_ants_gradient)
 
-if __name__ == '__main__':
-    # check backward
-    # N = 32
-    # img1 = torch.randn(1, 1, N, N, N).cuda()
-    # img2 = torch.randn(1, 1, N, N, N).cuda().requires_grad_(True)
-    # # tensor = img2 
-    # # print(tensor.shape)
-    # # print(tensor.is_contiguous())
-    # # print(tensor.storage().data_ptr())  # Check memory address
-    # # print(tensor.is_contiguous(memory_format=torch.contiguous_format))
-    # # print("--------------------------------")
 
-    # loss = FusedLocalNormalizedCrossCorrelationLoss(3, kernel_size=3, reduction='mean').cuda()
-    # out = loss(img1, img2)
-    # print(out)
-    # out.backward()
-    # print(img2.grad)
-    # sys.exit()
-
+def test_fused_cc_fwd_and_mem():
     for i in range(4, 10):
+    # torch.cuda.memory._record_memory_history()
+    # for i in range(6, 7):
         N = 2 ** i
         img1 = torch.rand(1, 1, N, N, N).cuda()
-        img2 = torch.rand(1, 1, N, N, N).cuda()
+        img2 = torch.rand(1, 1, N, N, N).cuda().requires_grad_(True)
         
         # Calculate input tensor memory
         input_memory = (img1.element_size() * img1.nelement() + 
@@ -175,6 +189,8 @@ if __name__ == '__main__':
         start = time()
         out_baseline2 = fast_lncc(img1, img2, 3)
         torch.cuda.synchronize()
+        # out_baseline2.backward()
+        # torch.cuda.synchronize()
         out_baseline2_time = time() - start
         baseline2_memory = (torch.cuda.max_memory_allocated() / 1024**2) - input_memory  # Subtract input memory
 
@@ -189,6 +205,119 @@ if __name__ == '__main__':
         print(f"out_time2: {out_baseline2_time:.4f}, out_baseline_time2: {out_baseline2_time:.4f}, speed up: {out_baseline2_time / out_time:.2f}x")
         print(f"Input memory: {input_memory:.2f}MB")
         print(f"Fused memory (excluding input): {fused_memory:.2f}MB, Baseline memory (excluding input): {baseline_memory:.2f}MB, Baseline2 memory (excluding input): {baseline2_memory:.2f}MB")
-        print(f"Memory reduction: {baseline_memory/fused_memory:.4f}x, {baseline2_memory/fused_memory:.4f}x")
+        print(f"Memory reduction: {(baseline_memory - fused_memory)/baseline_memory:.4f}%, {(baseline2_memory - fused_memory)/baseline2_memory:.4f}%")
         print()
 
+def test_fused_cc_bwd_and_mem():
+    ''' check backward memory usage '''
+    for i in range(4, 10):
+    # torch.cuda.memory._record_memory_history()
+    # for i in range(8, 9):
+        N = 2 ** i
+        eps = 1e-1
+        img1 = (torch.rand(1, 1, N, N, N) + eps).cuda().requires_grad_(True)
+        img2 = (torch.rand(1, 1, N, N, N) + eps).cuda().requires_grad_(True) 
+        
+        # Calculate input tensor memory
+        input_memory = (img1.element_size() * img1.nelement() + 
+                       img2.element_size() * img2.nelement()) / 1024**2  # MB
+        
+        loss = FusedLocalNormalizedCrossCorrelationLoss(3, kernel_size=3, reduction='mean', use_ants_gradient=False).cuda()
+        loss_baseline = LocalNormalizedCrossCorrelationLoss(3, kernel_size=3, reduction='mean').cuda()
+        
+        # Reset memory stats
+        torch.cuda.reset_peak_memory_stats()
+        
+        t0 = time()
+        out = -loss(img1, img2)
+        torch.cuda.synchronize()
+        t1 = time()
+        out.backward()
+        torch.cuda.synchronize()
+        t2 = time()
+        fwd_time_ours = t1 - t0
+        bwd_time_ours = t2 - t1
+        grad_ours = img2.grad / N**3
+        fused_memory = (torch.cuda.max_memory_allocated() / 1024**2) - input_memory  # Subtract input memory
+
+        img2.grad = None
+        # Reset memory stats again
+        torch.cuda.reset_peak_memory_stats()
+        
+        t0 = time()
+        out_baseline = loss_baseline(img1, img2)
+        torch.cuda.synchronize()
+        t1 = time()
+        out_baseline.backward()
+        torch.cuda.synchronize()
+        t2 = time()
+        fwd_time_baseline = t1 - t0
+        bwd_time_baseline = t2 - t1
+        grad_baseline = img2.grad
+        baseline_memory = (torch.cuda.max_memory_allocated() / 1024**2) - input_memory  # Subtract input memory
+
+        # baseline 2
+        img2.grad = None
+        torch.cuda.reset_peak_memory_stats()
+        t0 = time()
+        out_baseline2 = fast_lncc(img1, img2, 3)
+        torch.cuda.synchronize()
+        t1 = time()
+        out_baseline2.backward()
+        torch.cuda.synchronize()
+        t2 = time()
+        fwd_time_baseline2 = t1 - t0
+        bwd_time_baseline2 = t2 - t1
+        grad_baseline2 = img2.grad
+        baseline2_memory = (torch.cuda.max_memory_allocated() / 1024**2) - input_memory  # Subtract input memory
+
+        out = out.item()
+        out_baseline = out_baseline.item()
+        out_baseline2 = out_baseline2.item()
+        rel_err = abs(out - out_baseline) / abs(out_baseline)
+        rel_err2 = abs(out - out_baseline2) / abs(out_baseline2)
+        rel_grad_err = ((grad_ours - grad_baseline).abs() / (1e-7 + grad_baseline.abs())).mean()
+        rel_grad_err2 = ((grad_ours - grad_baseline2).abs() / (1e-7 + grad_baseline2.abs())).mean()
+        rel_grad_baselines = ((grad_baseline - grad_baseline2).abs() / (1e-7 + grad_baseline2.abs())).mean()
+
+        print(f"N: {N}, rel_err: {rel_err}, rel_err2: {rel_err2}")
+        print(f"rel_grad_err: {rel_grad_err}, rel_grad_err2: {rel_grad_err2}, rel_grad_baselines: {rel_grad_baselines}")
+        print()
+        print(f"out: {out}, out_baseline: {out_baseline}, out_baseline2: {out_baseline2}")
+
+        # compare fwd_time of ours and baselines, and speedup
+        print(f"fwd_time_ours: {fwd_time_ours:.4f}, fwd_time_baseline: {fwd_time_baseline:.4f}, speedup: {fwd_time_baseline / fwd_time_ours:.2f}x")
+        print(f"fwd_time_ours: {fwd_time_ours:.4f}, fwd_time_baseline2: {fwd_time_baseline2:.4f}, speedup: {fwd_time_baseline2 / fwd_time_ours:.2f}x")
+        print()
+
+        # same for backward time
+        print(f"bwd_time_ours: {bwd_time_ours:.4f}, bwd_time_baseline: {bwd_time_baseline:.4f}, speedup: {bwd_time_baseline / bwd_time_ours:.2f}x")
+        print(f"bwd_time_ours: {bwd_time_ours:.4f}, bwd_time_baseline2: {bwd_time_baseline2:.4f}, speedup: {bwd_time_baseline2 / bwd_time_ours:.2f}x")
+        print()
+
+        print(f"Input memory: {input_memory:.2f}MB")
+        print(f"Fused memory (excluding input): {fused_memory:.2f}MB, Baseline memory (excluding input): {baseline_memory:.2f}MB, Baseline2 memory (excluding input): {baseline2_memory:.2f}MB")
+        print(f"Memory reduction: {100*(baseline_memory - fused_memory)/baseline_memory:.4f}%, {100*(baseline2_memory - fused_memory)/baseline2_memory:.4f}%")
+        print("--------------------------------")
+    # torch.cuda.memory._dump_snapshot("fused_cc_bwd_mem.pkl")
+
+if __name__ == '__main__':
+    # check backward
+    # N = 32
+    # img1 = torch.randn(1, 1, N, N, N).cuda()
+    # img2 = torch.randn(1, 1, N, N, N).cuda().requires_grad_(True)
+    # # tensor = img2 
+    # # print(tensor.shape)
+    # # print(tensor.is_contiguous())
+    # # print(tensor.storage().data_ptr())  # Check memory address
+    # # print(tensor.is_contiguous(memory_format=torch.contiguous_format))
+    # # print("--------------------------------")
+
+    # loss = FusedLocalNormalizedCrossCorrelationLoss(3, kernel_size=3, reduction='mean').cuda()
+    # out = loss(img1, img2)
+    # print(out)
+    # out.backward()
+    # print(img2.grad)
+    # sys.exit()
+    # test_fused_cc_fwd_and_mem()
+    test_fused_cc_bwd_and_mem()
--- fused_ops/src/CrossCorrelation.cu
+++ fused_ops/src/CrossCorrelation.cu
@@ -11,6 +11,7 @@
 
 // C++ standard headers
 #include <cmath>
+// #include <math.h>
 #include <assert.h>
 
 // Local headers
@@ -96,12 +97,118 @@ __global__ void cc3d_fwd_interm_v1_kernel(
     }
 }
 
+/*
+This kernel computes the quantities required to compute the gradients of the input and target images.
+
+Modifying the intermediates in-place will save memory, and depending on the value of `compute_grad_target`
+we compute the other quantities.
+
+mean(I) = mu
+mean(J) = rho
+mean(I**2) = mu2
+mean(J**2) = rho2
+mean(IJ) = IJ
+
+A = k * (IJ - mu * rho)
+B = k * (mu2 - mu**2)
+C = k * (rho2 - rho**2)
+
+In intermediates, we have (mu, rho, mu2, rho2, IJ) for each channel.
+
+*/
 template <typename scalar_t>
-__global__ void cc3d_bwd_interm_v1_kernel_reduced(
-    scalar_t* __restrict__ interm, scalar_t* __restrict__ out, 
-    int kernel_volume, Reduction reduce, 
-    int batch_size, int n_out_channels, int height, int width, int depth, 
-    int out_size, float nr, float dr) {
+__global__ void cc3d_bwd_modify_interm_v1_kernel(
+    scalar_t* __restrict__ interm, scalar_t* __restrict__ input_img, scalar_t* __restrict__ target_img, 
+    scalar_t* __restrict__ grad_input_img, scalar_t* __restrict__ grad_target_img, scalar_t* __restrict__ grad_output,
+    int kernel_size, float nr, float dr, Reduction reduction, bool compute_grad_input, bool compute_grad_target,
+    int batch_size, int n_out_channels, int height, int width, int depth, scalar_t grad_output_val) {
+
+    float kernel_volume = (float)kernel_size;
+    kernel_volume = powf(kernel_volume, 3);
+
+    // input image dimensions
+    int n = batch_size * n_out_channels * height * width * depth;
+
+    CUDA_KERNEL_LOOP(i, n) {
+        int b = i / (n_out_channels * height * width * depth);
+        int c = (i / (height * width * depth)) % n_out_channels;
+        int h = (i / (width * depth)) % height;
+        int w = (i / depth) % width;
+        int d = i % depth;
+
+        int input_idx_common = d + depth * (w + width * (h + height * (5 * n_out_channels * b)));
+        int spatial_mult = depth * width * height;
+
+        // get intermediate values
+        scalar_t Ival = interm[input_idx_common + c * spatial_mult];
+        scalar_t Jval = interm[input_idx_common + (c + n_out_channels) * spatial_mult];
+        scalar_t I2val = interm[input_idx_common + (c + 2 * n_out_channels) * spatial_mult];
+        scalar_t J2val = interm[input_idx_common + (c + 3 * n_out_channels) * spatial_mult];
+        scalar_t IJval = interm[input_idx_common + (c + 4 * n_out_channels) * spatial_mult];
+
+        // compute intermediate values
+        scalar_t aval = kernel_volume * (IJval - Ival * Jval);
+        scalar_t bval = kernel_volume * (I2val - Ival * Ival);
+        scalar_t cval = kernel_volume * (J2val - Jval * Jval);
+
+        // compute output gradient
+        scalar_t gOVal = reduction == Reduction::NONE ? grad_output[i] : grad_output_val;
+        gOVal = 2 * gOVal * aval / (bval * cval + dr);    // 2gn / A = D
+
+        // add this term for further use
+        bval += dr;
+        cval += dr;
+
+        // write the first three entries in the intermediate array 
+        interm[input_idx_common + c * spatial_mult] = gOVal;   // D
+        interm[input_idx_common + (c + n_out_channels) * spatial_mult] = gOVal * aval / bval;
+        interm[input_idx_common + (c + 2 * n_out_channels) * spatial_mult] = gOVal * (aval / bval * Ival - Jval);
+        if (compute_grad_target) {
+            interm[input_idx_common + (c + 3 * n_out_channels) * spatial_mult] = gOVal * aval / cval;
+            interm[input_idx_common + (c + 4 * n_out_channels) * spatial_mult] = gOVal * (aval / cval * Jval - Ival);
+        }
+        
+    } // cuda kernel loop
+}
+
+template <typename scalar_t>
+__global__ void cc3d_bwd_compute_grads_kernel(
+    scalar_t* __restrict__ interm, scalar_t* __restrict__ input_img, scalar_t* __restrict__ target_img, 
+    scalar_t* __restrict__ grad_input_img, scalar_t* __restrict__ grad_target_img,
+    int batch_size, int n_out_channels, int height, int width, int depth) {
+
+    int n = batch_size * n_out_channels * height * width * depth;
+    CUDA_KERNEL_LOOP(i, n) {
+        int b = i / (n_out_channels * height * width * depth);
+        int c = (i / (height * width * depth)) % n_out_channels;
+        int h = (i / (width * depth)) % height;
+        int w = (i / depth) % width;
+        int d = i % depth;
+
+        // get intermediate values
+        int input_idx_common = d + depth * (w + width * (h + height * (5 * n_out_channels * b)));
+        int spatial_mult = depth * width * height;
+
+        // get image values  
+        scalar_t Ival = input_img[i];
+        scalar_t Jval = target_img[i];
+
+        scalar_t gini_a = interm[input_idx_common + c * spatial_mult];
+        scalar_t gini_b = interm[input_idx_common + (c + n_out_channels) * spatial_mult];
+        scalar_t gini_mu1 = interm[input_idx_common + (c + 2 * n_out_channels) * spatial_mult];
+
+        // compute grad_input
+        grad_input_img[i] = gini_a * Jval - gini_b * Ival + gini_mu1;
+
+        if (grad_target_img != nullptr) {
+            // get gini_
+            scalar_t gini_c = interm[input_idx_common + (c + 3 * n_out_channels) * spatial_mult];
+            scalar_t gini_mu2 = interm[input_idx_common + (c + 4 * n_out_channels) * spatial_mult];
+
+            // compute grad_target
+            grad_target_img[i] = gini_a * Ival - gini_c * Jval + gini_mu2;
+        }
+    }
 }
 
 torch::Tensor cc3d_fwd_interm_v1(torch::Tensor intermediates, int kernel_volume, Reduction reduction, float nr, float dr) {
@@ -163,7 +270,7 @@ torch::Tensor cc3d_fwd_interm_v1(torch::Tensor intermediates, int kernel_volume,
     return out;
 }
 
-torch::Tensor cc3d_bwd_interm_v1(torch::Tensor intermediates, torch::Tensor input_img, torch::Tensor target_img, 
+torch::Tensor cc3d_bwd_modify_interm_v1(torch::Tensor intermediates, torch::Tensor input_img, torch::Tensor target_img, 
     torch::Tensor grad_output, 
     std::optional<torch::Tensor> grad_input_img, std::optional<torch::Tensor> grad_target_img, 
     int kernel_size, float nr, float dr, Reduction reduction) {
@@ -178,9 +285,13 @@ torch::Tensor cc3d_bwd_interm_v1(torch::Tensor intermediates, torch::Tensor inpu
     input_img: [batch_size, n_channels, height, width, depth]
     target_img: [batch_size, n_channels, height, width, depth]
     grad_output: [batch_size, n_out_channels, height, width, depth]
-    grad_input_img: [batch_size, n_channels, height, width, depth]
-    grad_target_img: [batch_size, n_channels, height, width, depth]
+    grad_input_img: [batch_size, n_channels, height, width, depth] (optional)
+    grad_target_img: [batch_size, n_channels, height, width, depth] (optional)
     */
+
+    if (!grad_input_img.has_value() && !grad_target_img.has_value()) {
+        throw std::runtime_error("At least one of grad_input_img or grad_target_img must be provided");
+    }
 
     auto n = intermediates.numel() / 5;
     int batch_size = intermediates.size(0);
@@ -202,32 +313,74 @@ torch::Tensor cc3d_bwd_interm_v1(torch::Tensor intermediates, torch::Tensor inpu
         CHECK_INPUT(grad_target_img.value());
     }
 
-    if (reduction == Reduction::NONE) {
-        throw std::runtime_error("NONE reduction not supported for backward pass yet");
-    }
-    else {
+    // for mean, dont normalize the gradient yet (let's see if more stable gradients can be achieved)
+    // if (reduction == Reduction::MEAN) {
+    //     grad_output_val = grad_output_val / n;
+    // }
+    dim3 blockSize(BLOCKSIZE_3D);
+    dim3 gridSize((n + blockSize.x - 1) / blockSize.x);
+
+    bool compute_grad_input = grad_input_img.has_value();
+    bool compute_grad_target = grad_target_img.has_value();
+
+    // this 
+    TORCH_CHECK(compute_grad_input, "compute_grad_input must be true - why would you call this function otherwise?");
+
+    AT_DISPATCH_FLOATING_TYPES_AND2(at::ScalarType::Half, at::ScalarType::BFloat16, intermediates.scalar_type(), "cc3d_bwd_interm_v1", ([&] {
         // initialize output tensors
-        float grad_output_val = grad_output.cpu().item<float>();
-        // dont do it yet (let's see if more stable gradients can be achieved)
-        // if (reduction == Reduction::MEAN) {
-        //     grad_output_val = grad_output_val / n;
-        // }
-        dim3 blockSize(BLOCKSIZE_3D);
-        dim3 gridSize((n + blockSize.x - 1) / blockSize.x);
+        scalar_t grad_output_val = 1;
+        if (reduction == Reduction::NONE) {
+            // do nothing
+        }
+        else {
+            // only a single value, extract it
+            grad_output_val = grad_output.cpu().item<scalar_t>();
+        }
+        // initialize output gradient pointers if they exist
+        scalar_t* grad_input_img_ptr = grad_input_img.has_value() ? grad_input_img.value().data_ptr<scalar_t>() : nullptr;
+        scalar_t* grad_target_img_ptr = grad_target_img.has_value() ? grad_target_img.value().data_ptr<scalar_t>() : nullptr;
 
-        // AT_DISPATCH_FLOATING_TYPES_AND2(at::ScalarType::Half, at::ScalarType::BFloat16, intermediates.scalar_type(), "cc3d_bwd_interm_v1", ([&] {
-        //     // initialize output gradient pointers if they exist
-        //     scalar_t* grad_input_img_ptr = grad_input_img.has_value() ? grad_input_img.value().data_ptr<scalar_t>() : nullptr;
-        //     scalar_t* grad_target_img_ptr = grad_target_img.has_value() ? grad_target_img.value().data_ptr<scalar_t>() : nullptr;
+        cc3d_bwd_modify_interm_v1_kernel<scalar_t><<<gridSize, blockSize, 0, at::cuda::getCurrentCUDAStream()>>>(
+            intermediates.data_ptr<scalar_t>(), input_img.data_ptr<scalar_t>(), target_img.data_ptr<scalar_t>(),  
+            grad_input_img_ptr, grad_target_img_ptr, grad_output.data_ptr<scalar_t>(),
+            kernel_size, nr, dr, reduction, compute_grad_input, compute_grad_target,
+            batch_size, n_out_channels, height, width, depth, grad_output_val);
+    }));
+    
+    return torch::Tensor();
+}
 
-        //     cc3d_bwd_interm_v1_kernel_reduced<scalar_t><<<gridSize, blockSize, 0, at::cuda::getCurrentCUDAStream()>>>(
-        //         intermediates.data_ptr<scalar_t>(), input_img.data_ptr<scalar_t>(), target_img.data_ptr<scalar_t>(),  
-        //         grad_input_img_ptr, grad_target_img_ptr, kernel_size, nr, dr, reduction);
-        // }));
-        
-        std::cout << "grad output shape: " << grad_output.sizes() << std::endl;
-        return torch::Tensor();
-    }
+void cc3d_bwd_compute_grads(torch::Tensor intermediates, torch::Tensor input_img, torch::Tensor target_img, torch::Tensor grad_input_img, std::optional<torch::Tensor> grad_target_img) {
+    /*
+    intermediates: [batch_size, 5*n_channels, height, width, depth]
+    input_img: [batch_size, n_channels, height, width, depth]
+    target_img: [batch_size, n_channels, height, width, depth]
+    grad_input_img: [batch_size, n_channels, height, width, depth]
+    grad_target_img: [batch_size, n_channels, height, width, depth]
+    */
+    auto n = intermediates.numel() / 5;
+    int batch_size = intermediates.size(0);
+    int n_channels = intermediates.size(1);
+    int height = intermediates.size(2);
+    int width = intermediates.size(3);
+    int depth = intermediates.size(4);
+    assert(n_channels % 5 == 0);
+    int n_out_channels = n_channels / 5;   // one output channel for each block of 5 input channels
+
+    dim3 blockSize(BLOCKSIZE_3D);
+    dim3 gridSize((n + blockSize.x - 1) / blockSize.x);
+
+    AT_DISPATCH_FLOATING_TYPES_AND2(at::ScalarType::Half, at::ScalarType::BFloat16, intermediates.scalar_type(), "cc3d_bwd_compute_grads", ([&] {
+        // initialize output gradient pointers if they exist
+        scalar_t* grad_input_img_ptr = grad_input_img.data_ptr<scalar_t>();
+        scalar_t* grad_target_img_ptr = grad_target_img.has_value() ? grad_target_img.value().data_ptr<scalar_t>() : nullptr;
+
+        cc3d_bwd_compute_grads_kernel<scalar_t><<<gridSize, blockSize, 0, at::cuda::getCurrentCUDAStream()>>>(
+            intermediates.data_ptr<scalar_t>(), input_img.data_ptr<scalar_t>(), target_img.data_ptr<scalar_t>(),  
+            grad_input_img_ptr, grad_target_img_ptr, 
+            batch_size, n_out_channels, height, width, depth
+        );
+    }));
 }
 
 torch::Tensor create_intermediates(torch::Tensor input_img, torch::Tensor target_img, torch::Tensor intermediates) {

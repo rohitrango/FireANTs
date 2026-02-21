86efb35764040208e3df631d5b2f0981f9f74d89
rohitrango <rohit.rango@gmail.com>
2026-02-21 18:02:38 -0500

    draft report
    Signed-off-by: rohitrango <rohit.rango@gmail.com>

---
+++ reports/genericlabel/benchmark_genericlabel_registration.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Placeholder for minimal GenericLabel registration experiment.
+
+Intended to run a few iterations of single-scale registration with a loss on
+warped labels and save figures/registration_loss_curve.pdf.
+Requires: CUDA, fireants_fused_ops. To be implemented when running experiments.
+"""
+import sys
+print("Placeholder: registration experiment script not implemented.")
+sys.exit(0)
